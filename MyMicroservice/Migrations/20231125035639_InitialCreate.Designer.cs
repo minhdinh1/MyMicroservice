c984b7e110bf583e0e0d2ae592209e55a2e4d7a5
Minh Dinh <minh.dinh@gluwa.com>
2023-11-27 16:39:24 -0800

    Implement crud actions to interact with db

--- MyMicroservice/Migrations/20231125035639_InitialCreate.Designer.cs
+++ MyMicroservice/Migrations/20231125035639_InitialCreate.Designer.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using MyMicroservice.DbContextSpace;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
